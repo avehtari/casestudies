448a75e0f67b3473a4bf6d1e68d1e2b411bb482d
Aki Vehtari <Aki.Vehtari@aalto.fi>
2021-02-10 17:35:04 +0200

    add more explanations

--- Motorcycle/motorcycle_gpcourse.R
+++ Motorcycle/motorcycle_gpcourse.R
@@ -29,12 +29,19 @@
 #' Data are measurements of head acceleration in a simulated
 #' motorcycle accident, used to test crash helmets.
 #'
-#' Data are modelled with normal distribution having Gaussian process
+#' Data are modelled first with normal distribution having Gaussian process
+#' prior on mean:
+#' $$
+#' y \sim \mbox{normal}(f(x), \sigma)\\
+#' f \sim GP(0, K_1)\\
+#' \sigma \sim normal^{+}(0, 1),
+#' $$
+#' and then with normal distribution having Gaussian process
 #' prior on mean and log standard deviation:
 #' $$
-#' y \sim \mbox{normal}(\mu(x), \exp(\eta(x))\\
-#' \mu \sim GP(0, K_1)\\
-#' \eta \sim GP(0, K_2)
+#' y \sim \mbox{normal}(f(x), \exp(g(x))\\
+#' f \sim GP(0, K_1)\\
+#' g \sim GP(0, K_2).
 #' $$
 #'
 
@@ -51,7 +58,7 @@ library(ggplot2)
 library(bayesplot)
 theme_set(bayesplot::theme_default(base_family = "sans", base_size=16))
 set1 <- RColorBrewer::brewer.pal(7, "Set1")
-SEED <- 48927 # set random seed for reproducability
+SEED <- 48927 # set random seed for reproducibility
 
 #' ## Motorcycle accident acceleration data
 #' 
@@ -65,7 +72,17 @@ mcycle %>%
   geom_point()+
   labs(x="Time (ms)", y="Acceleration (g)")
 
-#' ## GP model with homoskedastic noise
+#' ## GP model with homoskedastic residual
+#'
+#' We start with a simpler homoskedastic residual Gaussian process model
+#' $$
+#' y \sim \mbox{normal}(f(x), \sigma)\\
+#' f \sim GP(0, K_1)\\
+#' \sigma \sim normal^{+}(0, 1),
+#' $$
+#' that has analytic marginal likelihood for the covariance function
+#' and residual scale parameters.
+#' 
 
 #' Model code
 file0 <- "gpcov0.stan"
@@ -101,6 +118,9 @@ cbind(mcycle,pred) %>%
   geom_line(aes(y=Ef-2*sigma), color=set1[1],linetype="dashed")+
   geom_line(aes(y=Ef+2*sigma), color=set1[1],linetype="dashed")
 
+#' The model fit given optimized parameters, looks reasonable
+#' considering the use of homoskedastic residual model.
+#' 
 #' Sample using dynamic HMC
 #+ fit0, results='hide'
 fit0 <- model0$sample(data=standata0, iter_warmup=500, iter_sampling=500,
@@ -123,7 +143,9 @@ cbind(mcycle,pred) %>%
   geom_line(aes(y=Ef-2*sigma), color=set1[1],linetype="dashed")+
   geom_line(aes(y=Ef+2*sigma), color=set1[1],linetype="dashed")
 
-
+#' The model fit given integrated parameters looks similar to the
+#' optimized one.
+#' 
 #' Compare the posterior draws to the optimized parameters
 optim<-as_draws_df(subset(odraws0, variable=c('sigma_f','lengthscale_f')))
 drawsdf<-as_draws_df(subset(draws0, variable=c('sigma_f','lengthscale_f')))%>%thin_draws(thin=5)
@@ -136,7 +158,11 @@ drawsdf%>%
   annotate("text",x=optim$lengthscale_f+0.01,y=optim$sigma_f,
            label='Optimized',hjust=0,color=set1[1],size=6)
 
-
+#' The optimization result is in the middle of the posterior and quite
+#' well representative of the low dimensional posterior (in higher
+#' dimensions the mean or mode of the posterior is not likely to be
+#' representative).
+#' 
 #' Compare optimized and posterior predictive distributions
 Efo <- as.numeric(subset(odraws0, variable='f'))
 sigmao <- as.numeric(subset(odraws0, variable='sigma'))
@@ -155,7 +181,16 @@ cbind(mcycle,pred) %>%
   geom_line(aes(y=Efo-2*sigmao), color=set1[2],linetype="dashed")+
   geom_line(aes(y=Efo+2*sigmao), color=set1[2],linetype="dashed")
 
+#' The model predictions are very similar, and in general GP
+#' covariance function and observation model parameters can be quite
+#' safely optimized if there are only a few of them and thus marginal
+#' posterior is low dimensional and the number of observations is
+#' relatively high.
+#' 
 #' ### 10% of data
+#'
+#' To demonstrate that the optimization is not always safe, we use
+#' only 10% of the data for model fitting.
 #' 
 #' Data to be passed to Stan
 mcycle00 <- mcycle[seq(1,133,by=10),]
@@ -184,6 +219,8 @@ pred<-data.frame(Ef=Ef,sigma=sigma)
   geom_line(data=mcycle,aes(x=times,y=Ef-2*sigma), color=set1[1],linetype="dashed")+
   geom_line(data=mcycle,aes(x=times,y=Ef+2*sigma), color=set1[1],linetype="dashed")
 
+#' The model fit is clearly over-fitted and over-confident.
+#' 
 #' Sample using dynamic HMC
 #+ fit00, results='hide'
 fit00 <- model0$sample(data=standata00, iter_warmup=1000, iter_sampling=1000,
@@ -206,6 +243,10 @@ pred<-data.frame(Ef=Ef,sigma=sigma)
   geom_line(data=mcycle,aes(x=times,y=Ef-2*sigma), color=set1[1],linetype="dashed")+
   geom_line(data=mcycle,aes(x=times,y=Ef+2*sigma), color=set1[1],linetype="dashed")
 
+#' The posterior predictive distribution is much more conservative and
+#' shows the uncertainty due to having only a small number of
+#' observations.
+#' 
 #' Compare the posterior draws to the optimized parameters
 optim<-as_draws_df(subset(odraws00, variable=c('sigma','sigma_f','lengthscale_f')))
 drawsdf<-as_draws_df(subset(draws00, variable=c('sigma','sigma_f','lengthscale_f')))%>%thin_draws(thin=5)
@@ -218,6 +259,11 @@ drawsdf%>%
   annotate("text",x=(optim$lengthscale_f+0.01),y=(optim$sigma),
            label='Optimized',hjust=0,color=set1[1],size=6)
 
+#' The optimization result is in the edge of the posterior close to
+#' zero residual scale. While there are posterior draws close to zero,
+#' integrating over the wide posterior takes into account the
+#' uncertainty and the predictions thus are more uncertain, too.
+#' 
 #' Compare optimized and posterior predictive distributions
 Efo <- as.numeric(subset(odraws00, variable='f'))
 sigmao <- as.numeric(subset(odraws00, variable='sigma'))
@@ -235,8 +281,31 @@ cbind(mcycle,pred) %>%
   geom_line(aes(y=Efo-2*sigmao), color=set1[2],linetype="dashed")+
   geom_line(aes(y=Efo+2*sigmao), color=set1[2],linetype="dashed")
 
+#' The figure shows the model prediction given 10% of data, but also
+#' the full data as test data. The optimized model is over-fitted and
+#' overconfident. Even if the homoskedastic residual is wrong here,
+#' the posterior predictive interval covers most of the observation
+#' (and in case of good calibration should not cover them all).
+#' 
 
 #' ## GP with covariance matrices
+#'
+#' We next make a model with heteroskedastic residual model using
+#' Gaussian process prior also for the logarithm of the residual
+#' scale:
+#' $$
+#' y \sim \mbox{normal}(f(x), \exp(g(x))\\
+#' f \sim GP(0, K_1)\\
+#' g \sim GP(0, K_2).
+#' $$
+#'
+#' Now there is no analytical solution as GP prior through the
+#' exponential function is not a conjugate prior. In this case we
+#' present the latent values of f and g explicitly and sample from the
+#' joint posterior of the covariance function parameters, and the
+#' latent values. It would be possible also to use Laplace,
+#' variational inference, or expectation propagation to integrate over
+#' the latent values, but that is another story.
 #' 
 #' Model code
 file2 <- "gpcov.stan"
@@ -270,6 +339,10 @@ cbind(mcycle,pred) %>%
   geom_line(aes(y=Ef-2*sigma), color=set1[1],linetype="dashed")+
   geom_line(aes(y=Ef+2*sigma), color=set1[1],linetype="dashed")
 
+#' The optimization overfits, as we are now optimizing the joint
+#' posterior of 2 covariance function parameters and 2 x 133 latent
+#' values.
+#' 
 #' Sample using dynamic HMC
 #+ fit2, results='hide'
 fit2 <- model2$sample(data=standata2, iter_warmup=100, iter_sampling=100,
@@ -292,6 +365,8 @@ cbind(mcycle,pred) %>%
   geom_line(aes(y=Ef-2*sigma), color=set1[1],linetype="dashed")+
   geom_line(aes(y=Ef+2*sigma), color=set1[1],linetype="dashed")
 
+#' The MCMC integration works well and the model fit looks good.
+#' 
 #' Plot posterior draws and posterior mean of the mean function
 subset(draws2, variable="f") %>%
   as_draws_df() %>%
@@ -305,6 +380,11 @@ subset(draws2, variable="f") %>%
   geom_point(data=mcycle, mapping=aes(x=times,y=accel), inherit.aes=FALSE)+
   geom_line(data=cbind(mcycle,pred), mapping=aes(x=times,y=Ef), inherit.aes=FALSE, color=set1[1], size=1)
 
+#' We can also plot the posterior draws of the latent functions, which
+#' is a good reminder that individual draws are more wiggly than the
+#' average of the draws, and thus show better also the uncertainty,
+#' for example, in the edge of the data.
+#' 
 #' Compare the posterior draws to the optimized parameters
 optim<-as_draws_df(subset(odraws2, variable=c('sigma_g','lengthscale_g')))
 drawsdf<-as_draws_df(subset(draws2, variable=c('sigma_g','lengthscale_g')))
@@ -317,10 +397,21 @@ drawsdf%>%
   annotate("text",x=optim$lengthscale_g+0.01,y=optim$sigma_g,
            label='Optimized',hjust=0,color=set1[1],size=6)
 
-
+#' Optimization result is far from being representative of the
+#' posterior.
+#' 
 
 #' ## GP model with Hilbert basis functions
 #'
+#' The covariance matrix approach requires computation of Cholesky of
+#' the covariance matrix which has time cost O(n^3) and this is needs
+#' to be done every time the parameters change, which in case of MCMC
+#' can be quite many times and thus the computation time can be
+#' significant when n grows. One way to speed up the computation in
+#' low dimensional covariate case is to use basis function
+#' approximation which changes the GP to a linear model. Here we use
+#' Hilbert space basis functions.
+#' 
 #' Code for illustrating the basis functions
 #' Model code
 filebf0 <- "gpbf0.stan"
@@ -340,8 +431,7 @@ standatabf0 <- list(x=seq(0,1,length.out=100),
                     lengthscale_f=1) 
 #' Generate basis functions
 fixbf0 <- modelbf0$sample(data=standatabf0, fixed_param=TRUE, iter=1, iter_sampling=1)
-#' There is certainly easier way to manipulate,
-#' but this is what I came up quickly
+#' There is certainly easier way to do this, but this is what I came up quickly
 q<-subset(fixbf0$draws(), variable="PHI_f") %>%
   as_draws_matrix() %>%
   as.numeric()%>%
@@ -399,7 +489,10 @@ cbind(mcycle,pred) %>%
   geom_line(aes(y=Ef-2*sigma), color=set1[1],linetype="dashed")+
   geom_line(aes(y=Ef+2*sigma), color=set1[1],linetype="dashed")
 
-
+#' The optimization overfits, as we are now optimizing the joint
+#' posterior of 2 covariance function parameters and 2 x 40 basis
+#' function co-efficients.
+#' 
 #' Sample using dynamic HMC
 #+ fit1, results='hide'
 fit1 <- model1$sample(data=standata1, iter_warmup=500, iter_sampling=500,
@@ -422,6 +515,8 @@ cbind(mcycle,pred) %>%
   geom_line(aes(y=Ef-2*sigma), color=set1[1],linetype="dashed")+
   geom_line(aes(y=Ef+2*sigma), color=set1[1],linetype="dashed")
 
+#' The MCMC integration works well and the model fit looks good.
+#' 
 #' Plot posterior draws and posterior mean of the mean function
 subset(draws1, variable="f") %>%
   thin_draws(thin=5)%>%
@@ -436,6 +531,11 @@ subset(draws1, variable="f") %>%
   geom_point(data=mcycle, mapping=aes(x=times,y=accel), inherit.aes=FALSE)+
   geom_line(data=cbind(mcycle,pred), mapping=aes(x=times,y=Ef), inherit.aes=FALSE, color=set1[1], size=1)
 
+#' We can also plot the posterior draws of the latent functions, which
+#' is a good reminder that individual draws are more wiggly than the
+#' average of the draws, and thus show better also the uncertainty,
+#' for example, in the edge of the data.
+#' 
 #' Compare the posterior draws to the optimized parameters
 optim<-as_draws_df(subset(odraws1, variable=c('sigma_g','lengthscale_g')))
 optim<-as_draws_df(subset(odraws1, variable=c('sigma_g','lengthscale_g')))
@@ -449,3 +549,6 @@ drawsdf%>%
   annotate("text",x=optim$lengthscale_g+0.01,y=optim$sigma_g,
            label='Optimized',hjust=0,color=set1[1],size=6)
 
+#' Optimization result is far from being representative of the
+#' posterior.
+#' 
